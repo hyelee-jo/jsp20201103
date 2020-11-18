db0431023d160bb7eb9071bc74ee28ae89ba50ca
harbor_@naver.com <harbor_@naver.com>
2020-11-18 16:43:39 +0900

    201118

---
+++ WebContent/WEB-INF/sql/select/subEx1.sql
@@ -0,0 +1,97 @@
+SELECT manager FROM employee
+WHERE eno = '7499';
+
+SELECT ename FROM employee
+WHERE eno = 7698;
+
+SELECT ename FROM employee
+WHERE eno = (SELECT manager
+            FROM employee
+            WHERE eno = '7499');
+
+select ename, dno
+from employee
+where dno = (select dno
+            from employee
+            where ename='SCOTT');
+            
+select ename, job, salary
+from employee
+where salary = (select min(salary)
+                from employee);
+                
+select dno, min(salary)
+from employee
+group by dno
+having min(salary) > (select min(salary)
+                     from employee
+                     where dno=30);
+                     
+SELECT ename FROM employee
+WHERE salary > (SELECT salary
+               FROM employee
+               WHERE eno = 7499);
+                     
+SELECT ename FROM employee
+WHERE salary > (SELECT salary
+               FROM employee);
+
+--다중 행 서브 쿼리
+/*
+IN        값이 하나라도 있으면
+ANY(SOME) 하나 이상이 만족하면
+ALL       모두 만족하면
+*/
+
+SELECT dno 
+FROM department
+WHERE dno <= 20;
+
+SELECT ename, dno
+FROM employee
+WHERE dno IN (SELECT dno 
+             FROM department
+             WHERE dno <= 20);
+
+select eno, ename
+from employee
+where (dno, salary) in (select dno, min(salary)
+                from employee
+                group by dno);
+
+SELECT dno, MIN(salary)
+FROM employee
+GROUP By dno;
+
+--1)
+--2)
+--3)
+--4)
+--5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
